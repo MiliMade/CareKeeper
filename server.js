6650163dd041e5433623e5ccece36ff1f93bb599
MiliMade <milimadeemail@gmail.com>
2023-10-03 11:01:22 -0400

    initial commit - setup up environment

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from "express"
+import mongoose from "mongoose"
+import methodOverride from "method-override"
+import path from "path"
+import {dirname} from "path"
+import { fileURLToPath } from "url"
+
+const app = express();
+const {Schema, model} = mongoose;
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+const PORT = 8000
+
+app.set('views', path.join(__dirname, "views"))
+app.set("view engine", "ejs")
+app.set("layouts", "layouts/layout")
+
+app.use(express.urlencoded({extended:true}))
+app.use(methodOverride("_method"))
+app.use(express.static("public"))
+
+app.listen(PORT, ()=>{
+  console.log(`Whoa! Your server is totally running on port ${PORT}`)
+})
